ee7bcc7d3ee4f8647358436222f706bb6c075917
sethbilliau <sethbilliau@gmail.com>
2019-11-29 21:12:13 -0500

    reformat data

--- project.R
+++ project.R
@@ -3,8 +3,12 @@
 library(tidyverse)
 library(dplyr)
 
+teams34 <- read.csv('data/teams0304.csv')
+teams45 <- read.csv('data/teams0405.csv')
+teams56 <- read.csv('data/teams0506.csv')
+teams67 <- read.csv('data/teams0607.csv')
 teams78 <- read.csv('data/teams0708.csv')
 teams89 <- read.csv('data/teams0809.csv')
 
-teams78$School
-"Air Force"
+names(teams34)
+predictors <- c("")
